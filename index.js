12b378f3684eafb20c4b865f80de35bd47928218
João Victor (JV) <jcorrea@zak.app>
2022-01-10 16:27:37 -0300

    chore: add some instructions to access the swagger

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const init = async () => {
   try {
     const serverInstance = await server
     await serverInstance.start()
+    
     console.log(`API is running at ${config.app.port}`)
+    console.log('Check out the routes in the auto generated swagger')
+    console.log(`http://localhost:${config.app.port}/documentation`)
   } catch (error) {
     console.log({ message: `API failed to start ${error.message}` })
   }
--- v1/controllers/index.js
+++ v1/controllers/index.js
@@ -4,6 +4,9 @@ const coreWrapper = require('../core')
 const healthCheckController = require('./health-check-controller')
 const config = require('../../config')
 
+// inject the dependencies inside the core
+// this make the tests easier to write
+// cause you can override depencies that make use of IO process
 const core = coreWrapper({
   config,
 })
